521ac287a281c7fc02247e3ca5a1abc42b90f324
神樹桜乃 <kodama@sakuno.moe>
2017-11-03 16:08:36 +0800

    Asynchronously wait for process to exit

---
+++ src/Sakuno.SystemLayer/ProcessExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Sakuno.SystemLayer
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class ProcessExtensions
+    {
+        public static Task<int> WaitForExitAsync(this Process process)
+        {
+            if (process == null)
+                throw new ArgumentNullException(nameof(process));
+
+            var tcs = new TaskCompletionSource<int>();
+
+            process.EnableRaisingEvents = true;
+            process.Exited += OnProcessExited;
+
+            if (process.HasExited)
+                tcs.TrySetResult(process.ExitCode);
+
+            return tcs.Task;
+
+            void OnProcessExited(object sender, EventArgs e)
+            {
+                process.Exited -= OnProcessExited;
+
+                tcs.TrySetResult(process.ExitCode);
+            }
+        }
+    }
+}
