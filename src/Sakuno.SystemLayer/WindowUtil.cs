9b488f6045fa5cadf8369807a56daa94ccd960fb
神樹桜乃 <kodama@sakuno.moe>
2019-08-09 23:56:26 +0800

    Add WindowUtil

---
+++ src/Sakuno.SystemLayer/WindowUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sakuno.SystemLayer
+{
+    public static class WindowUtil
+    {
+        public static unsafe string GetClassName(IntPtr windowHandle)
+        {
+            Span<char> span = stackalloc char[256];
+            int length;
+
+            fixed (char* buffer = &span.GetPinnableReference())
+                length = NativeMethods.User32.GetClassNameW(windowHandle, buffer, span.Length);
+
+#if NETCOREAPP3_0
+            return new string(span[..length]);
+#else
+            return span.Slice(0, length).ToString();
+#endif
+        }
+        public static unsafe string GetWindowText(IntPtr windowHandle)
+        {
+            Span<char> span = stackalloc char[256];
+            int length;
+
+            fixed (char* buffer = &span.GetPinnableReference())
+                length = NativeMethods.User32.GetWindowTextW(windowHandle, buffer, span.Length);
+
+#if NETCOREAPP3_0
+            return new string(span[..length]);
+#else
+            return span.Slice(0, length).ToString();
+#endif
+        }
+    }
+}
